89da33e3a69a784ed50fb24d251b947d22b73f50
Niccolo Tesi <tesinicco@gmail.com>
2024-09-17 13:26:20 +0200

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -3,6 +3,7 @@ from email.mime.text import MIMEText
 import sys
 
 # read arguments -- username and random number
+print('** Hello!!')
 username = sys.argv[1]
 random_number = sys.argv[2]
 print(username)
